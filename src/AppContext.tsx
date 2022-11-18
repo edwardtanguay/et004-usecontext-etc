2d7a66ef7f5823e52534779c5c296442de11d209
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-18 11:28:13 +0100

    getting async books

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from 'react';
 import { createContext } from 'react';
+import axios from 'axios';
+
+const booksUrl = 'https://edwardtanguay.vercel.app/share/techBooks.json';
+
+interface IBook {
+	id: number;
+	idCode: string;
+	title: string;
+	description: string;
+	language: string;
+}
 
 interface IAppContext {
 	appTitle: string;
+	books: IBook[]
 }
 
 interface IAppProvider {
@@ -11,12 +24,31 @@ interface IAppProvider {
 export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
-	
+	const [books, setBooks] = useState<IBook[]>([]);
 	const appTitle = "The Study Group";
+
+	useEffect(() => {
+		(async () => {
+			const rawBooks = (await axios.get(booksUrl)).data;
+			const _books:IBook[] = [];
+			rawBooks.forEach((rawBook: any) => {
+				const _book: IBook = {
+					id: rawBook.id,
+					idCode: rawBook.idCode,
+					title: rawBook.title,
+					description: rawBook.description,
+					language: rawBook.language === '' ? 'English' : 'French'
+				}
+				_books.push(_book);
+			});
+			setBooks(_books);
+		})();
+	}, []);
 	
 	return (
 		<AppContext.Provider value={{
-			appTitle
+			appTitle,
+			books
 		}}>
 			{children}
 		</AppContext.Provider>
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
+import { AppContext } from '../AppContext';
+
 export const PageWelcome = () => {
+	const { books } = useContext(AppContext);
 	return (
 		<>
 			<p>Welcome to this site.</p>
+			<p>Our club is reading {books.length} books.</p>
 		</>
 	);
 };
